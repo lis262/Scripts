e2c59320cb79b0bfb6f30e67f1b0af4f3914b896
Li <lis262@amrndhl1316.pfizer.com>
2020-01-23 16:44:30 -0500

    upload vep vcf to tab script

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-* get_rna_pos_from_gff.py
-    This file extract gene/transcript position from gff file.
-    How to run: python get_rna_pos_from_gff.py -i input.gff -o output.txt
+get_rna_pos_from_gff.py
+-----------------------
+    - This file extract gene/transcript position from gff file.
+    - How to run: 
+    > python get_rna_pos_from_gff.py -i input.gff -o output.txt
+
+
+vep_vcf2tab_split_multi_alt.py
+------------------------------
+	- This file transfer vep vcf output format to tab format
+	- How to run:
+	> python vep_vcf2tab_split_multi_alt.py -i input_vcf_path -o output_tab_path
---
+++ vep_vcf2tab_split_multi_alt.py
@@ -0,0 +1,77 @@
+import glob
+import argparse
+
+'''
+This file transfer vep vcf output format to tab format.
+For multi-alt alleles, vep only use the 1st id, so here
+we make id mapping to the correct alternative id.
+'''
+
+parser = argparse.ArgumentParser(description='change vep vcf output to tab')
+parser.add_argument('-i','--vcf',action='store',dest='input',help='input vcf path')
+parser.add_argument('-o','--out',action='store',dest='output',help='output txt file')
+
+args = parser.parse_args()
+vcf_path = args.input
+tab_path = args.output
+
+
+
+
+def vep_vcf2tab(vcf, tab):
+    out_columns=['#Uploaded_variation','Location','Allele','Gene',
+                 'Feature Feature_type','Consequence','cDNA_position',
+                 'CDS_position','Protein_position','Amino_acids','Codons',
+                 'Existing_variation','Extra']
+
+    extra_fields=('IMPACT,STRAND,FLAGS,VARIANT_CLASS,HGVSc,HGVSp,LoF,LoF_info,'+ \
+                            'CAROL,Condel,gnomad_OE,LoFtool').split(',')
+    begin = True
+    with open(vcf) as in_vcf, open(tab,'w') as out:
+        # iterate each line in vcf format vep annotation
+        for line in in_vcf:
+            if line.startswith('##INFO=<ID=CSQ'): # extract CSQ column names
+                CSQ_keys = re.search('(?<=Format: ).+?(?=\">)', line).group(0).split('|')
+            elif line.startswith('#'): # other header
+                if line.startswith('##FILTER') or line.startswith('##INFO'):
+                    continue
+                else:
+                    out.write(line)
+            else:
+                if begin:
+                    out.write('\t'.join(out_columns) + '\n')
+                    begin = False
+                # split each column of vep annotation in vcf format
+                chrom,pos,vari_id,ref,alt,qual,filt,info,form=line.split('\t')
+                vari_ids = vari_id.split(';')
+                alts = alt.split(',')
+                # extract CSQ content
+                CSQ_items = re.search('(?<=CSQ=).+?(?=$)', info).group(0).split(',')
+                # iterate each annotation item in CSQ
+                for item in CSQ_items:
+                    CSQ_dict = {}
+                    CSQ_values = [i if i!='' else '-' for i in item.split('|')]
+                    for k,v in zip(CSQ_keys, CSQ_values):
+                        CSQ_dict[k] = v
+                    allele_num = int(CSQ_dict['ALLELE_NUM']) -1
+                    # generate tab output line
+                    out_line = [vari_ids[allele_num], pos, CSQ_dict['Allele'],
+                               CSQ_dict['Gene'],CSQ_dict['Feature'],CSQ_dict['Feature_type'],
+                               CSQ_dict['Consequence'],CSQ_dict['cDNA_position'],
+                               CSQ_dict['CDS_position'],CSQ_dict['Protein_position'],
+                               CSQ_dict['Amino_acids'],CSQ_dict['Codons'],
+                               CSQ_dict['Existing_variation']]
+                    extra_field = '' # last column in tab format
+                    for f in extra_fields:
+                        if CSQ_dict[f] != '-':
+                            extra_field = extra_field+f+'='+CSQ_dict[f]+';'
+                    out_line.append(extra_field[:-1])
+                    out.write('\t'.join(out_line) + '\n')
+
+
+# path = '/lustre/workspace/projects/IB/P19_014_UKBB_300K_Vep/data'
+vcfs = glob.glob(vcf_path + '/*.vcf')
+for vcf in vcfs:
+    tab = tab_path + '/' + vcf.split('/')[-1]
+    if not os.path.exists(tab):
+        vep_vcf2tab(vcf,tab)
